601cc75d02ae33d49f3e5dbf28cdc7f735a76135
kane <Kane.Li@movit-tech.com>
2025-04-24 09:15:46 +0800

    fix

--- CalcTest.ts
+++ CalcTest.ts
@@ -1,3 +1,3 @@
-import { uppercase } from './legacy-utils';
+import { uppercase } from './legacy-utils.ts';
 const result = uppercase('hello');  // TS 校验参数类型为 string，返回值为 string
 console.log(result);  // 输出："HELLO"
--- legacy-utils.ts
+++ legacy-utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Converts a string to uppercase
+ * @param str - The string to convert
+ * @returns The uppercase version of the string
+ */
 export function uppercase(str: string): string {
     return str.toUpperCase();
 } 
